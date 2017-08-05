119516b78539a1da0df71d41aa96872d505eeb94
raul9687 <30650599+raul9687@users.noreply.github.com>
2017-08-05 12:45:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_1',function(req,res){
-    res.send('Article 1 will be put up soon!');
+    res.sendFile(path.join(_dirname,'ui','article_1.html'));
 })
 
 app.get('/article_2',function(req,res){
