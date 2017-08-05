024152b5472804aa8040e1167628337d984e5e9f
raul9687 <30650599+raul9687@users.noreply.github.com>
2017-08-05 14:57:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,15 @@
 console.log('Loaded!');
+//Changing the content
 var div=document.getElementById('main');
 div.innerHTML='Updated Content';
+//Moving the image
+var img=document.getElementById('image');
+var temp=0;
+function moveRight()
+{
+    temp=temp+5;
+    img.style.marginLeft= temp + 'px';
+}
+img.onclick = function(){
+    var interval=setInterval(moveRight,100);
+};
