b3ba9b214c9e7682340c309b31ac3616603e77c0
raul9687 <30650599+raul9687@users.noreply.github.com>
2017-08-06 16:07:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,33 @@ button.onclick = function(){
     request.send(null);
     
 };
+
+var submit=document.getElementById('submit_btn');
+submit.onclick = function(){
+    
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    var commentInput=document.getElementById('comment');
+    var comment=commentInput.value;
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names=request.responseText;
+                var comments=request.responseText;
+                names=JSON.parse(names);
+                comments=JSON.parse(comments);
+                var list='';
+                for(var i=0; i< names.length(); i++){
+                    list += '<li>' + name[i] + '<br>' + comment[i] + '</li>';
+                }
+                var ul=document.getElementById('list');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    
+    request.open('GET','http://raul9687.imad.hasura-app.io/submit-comment?name=' + name + '&comment=' + comment,true);
+    request.send(null);
+};
