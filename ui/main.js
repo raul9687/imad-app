e96992add7be72e3463de04d81cc41aeb9439ebc
raul9687 <30650599+raul9687@users.noreply.github.com>
2017-08-07 08:32:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,3 @@
-var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var names=request.responseText;
-                var comments=request.responseText;
-                names=JSON.parse(names);
-                comments=JSON.parse(comments);
-                var list='';
-                for(var i=0; i< names.length; i++){
-                    list += '<li>' + names[i] + '<br>' + comments[i] + '</li>';
-                }
-                var ul=document.getElementById('list');
-                ul.innerHTML=list;
-            }
-        }
-    };
-    
-    request.open('GET','http://raul9687.imad.hasura-app.io/load',true);
-    request.send(null);
 
 var button=document.getElementById('counter');
 button.onclick = function(){
