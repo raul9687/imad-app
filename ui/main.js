ae32b33b7a930ae8426ea1773c1b6b33d331657a
raul9687 <30650599+raul9687@users.noreply.github.com>
2017-08-07 08:04:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function(){
                 comments=JSON.parse(comments);
                 var list='';
                 for(var i=0; i< names.length; i++){
-                    list += '<li>' + names[i] + '</li>';
+                    list += '<li>' + names[i] + '<br>' + comments[i] + '</li>';
                 }
                 var ul=document.getElementById('list');
                 ul.innerHTML=list;
