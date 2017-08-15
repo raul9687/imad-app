5bb94006e82a96d8d40700d0dbf1b8b2d6651666
raul9687 <30650599+raul9687@users.noreply.github.com>
2017-08-15 19:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var Pool = require('pg').Pool;
+var config = {
+    user: 'raul9687',
+    database: 'raul9687',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var articleOne={
     title: 'Article 1',
@@ -108,6 +116,22 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+var pool = new Pool(config); 
+app.get('/test-db', function(req,res){
+    
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err)
+        {
+            res.status('500').send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
+    
 });
 
 app.get('/ui/pp.jpg', function (req, res) {
